3c599ca0c58fc076008df78294c738b16b8aac6b
Ahilan Subbaian <asubbaia@stevens.edu>
2023-05-20 02:21:34 -0400

    logger level

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import logging
 from lambda_function import *
 import time
 start = time.time()
 
 print(f"Start time: {start}\n\n")
 logger = logging.getLogger(__name__)
-os.environ['LOG_LEVEL'] = 'INFO'
 
 testEvent = {
     'tickers': ["VONG", "SCHD", "SCHG", "SPGP", "RSP"],
--- test.py
+++ test.py
@@ -1,27 +1,15 @@
 import os
-from dotenv import load_dotenv
-import datetime
+import logging
 
+# Set the environment variable before calling logging.basicConfig()
+os.environ['LOG_LEVEL'] = 'ERROR'  # Replace with your desired log level
 
-def calculate_fridays(day_displace):
+# Retrieve the environment variable
+log_level = os.getenv('LOG_LEVEL', 'INFO')
 
-    # gets number of fridays [start, end)
-    def fridays_between(start, end):
-        fridays = 0
-        while start < end:
-            if start.weekday() == 4:
-                fridays += 1
-            start += datetime.timedelta(days=1)
-        return fridays
+# Configure logging
+logging.basicConfig(level=log_level)
 
-    # Get today's date
-    today = datetime.datetime.now()
-
-    # get displaced start date
-    end_date = datetime.datetime(
-        today.year, (today.month + (today.day >= day_displace)) % 12, day_displace)
-
-    return 0 if (days := fridays_between(today, end_date)) == 0 else 1 / days
-
-
-print(calculate_fridays(24))
+# Log some messages
+logging.debug('This is a debug log message')
+logging.info('This is an info log message')
