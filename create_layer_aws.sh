4e4b45b25284389a399d43646163ed0312adb1cc
Ahilan Subbaian <asubbaia@stevens.edu>
2023-05-27 03:03:15 -0400

    changed up layer creation

--- create_layer_aws.sh
+++ create_layer_aws.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# define the package name, directory and zip name
-PACKAGE="alpaca-py"
+# define the directory and zip name
 DIR="python"
-ZIP_NAME="$PACKAGE.zip"
+ZIP_NAME="packages.zip"
 
 # create a new virtual environment
 python3 -m venv venv
@@ -11,18 +10,22 @@ python3 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
-# install the package without its dependencies
-pip install $PACKAGE -t $DIR
+for PACKAGE in "$@"
+do
+  # install the package without its dependencies
+  pip install $PACKAGE -t $DIR
+done
 
 # deactivate the virtual environment
 deactivate
 
-# zip the package
+# zip the packages
 zip -r -9 $ZIP_NAME $DIR
 
 # create a new layer and upload the zip file
-# aws lambda publish-layer-version --layer-name $PACKAGE --zip-file fileb://$ZIP_NAME --compatible-runtimes python3.10
+aws lambda publish-layer-version --layer-name packages --zip-file fileb://$ZIP_NAME --compatible-runtimes python3.10
 
-# clean up: remove the venv directory and the package directory
+# clean up: remove the venv directory, the package directory and the zip file
 rm -rf venv
 rm -rf $DIR
+# rm $ZIP_NAME
