8078be57f65dfd4512d036122a1a6e3887c932a3
Ahilan Subbaian <asubbaia@stevens.edu>
2023-06-08 16:45:22 -0400

    added prod to update automatically also

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from lambda_function import *
 import time
 import json
 
-file_path = "event.json"
+file_path = "configs/alpaca_orders_schedule_DEV.json"
 with open(file_path, "r") as json_file:
-    testEvent = json.load(json_file)
+    testEvent = json.load(json_file)["Input"]
 testContext = {}
 logger = logging.getLogger(__name__)
 
