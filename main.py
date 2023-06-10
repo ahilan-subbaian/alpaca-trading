6dac64b4abd7ad55c4432d6f65001e3cff7f90bf
EC2 Default User <ec2-user@ip-172-31-91-90.ec2.internal>
2023-06-10 21:45:16 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,66 @@
 import logging
-from lambda_function import *
 import time
 import json
+import client
+import os
 
-file_path = "configs/alpaca_orders_schedule_DEV.json"
-with open(file_path, "r") as json_file:
-    testEvent = json.load(json_file)["Input"]
-testContext = {}
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M:%S')
 logger = logging.getLogger(__name__)
 
-start = time.time()
-print(f"Start time: {start}\n\n")
+def main():
+    file_path = "event.json"
+    with open(file_path, "r") as json_file:
+        event = json.load(json_file)
 
-lambda_handler(testEvent, testContext)
+    logger.info(f"Event: {event}")
 
-print(f"\n\nTotal time: {time.time() - start}")
+    only_keys = ['symbols', 'limit', 'paper']
+    if sorted(only_keys) != sorted(event.keys()):
+        logger.error(f"Event contains keys other than {only_keys}")
+        return {"result": False, "message": f"Event contains keys other than {only_keys}"}
+    
+    symbols = event["symbols"]
+    limit = event["limit"]
+    paper = event["paper"]
+
+    apiKey = os.getenv(f'API_KEY_{"PAPER" if paper else "LIVE"}')
+    secretKey = os.getenv(f'SECRET_KEY_{"PAPER" if paper else "LIVE"}')
+
+    print(apiKey, secretKey)
+
+    logger.info(
+        "API keys, Secret keys, symbols, limit and paper retrieved successfully")
+    
+    try:
+        connection = client.AlpacaClient(
+            apiKey=apiKey, secretKey=secretKey, symbols=symbols, limit=limit, paper=paper)
+    except Exception as e:
+        error_message = f"Error initializing trading client: {str(e)}"
+        return {"result": False, "message": error_message}
+    
+    logger.info("AlpacaClient initialized successfully")
+
+    try:
+        response = connection.execute()
+    except Exception as e:
+        error_message = f"Error executing trading client actions: {type(e).__name__}, {str(e)}"
+        return {"result": False, "message": error_message}
+    
+    logger.info(f"Response received: {response}")
+
+    return response
+
+if __name__ == "__main__":
+
+    start = time.time()
+    logger.info(f"Start time: {start}\n\n")
+
+    response = main()
+
+    if response["result"]:
+        logger.info(response["message"])
+    else:
+        logger.error(response["message"])
+
+    logger.info(f"\n\nTotal time: {time.time() - start}")
